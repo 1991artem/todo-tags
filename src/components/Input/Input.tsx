5b7ab96cd96f03cff868ce93ae7142faeb215c02
1991artem <ar_puz@mail.ru>
2022-11-21 18:43:30 +0300

    fix: changed tags algoritm

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { useRef, KeyboardEvent } from "react";
+import { useRef, KeyboardEvent, useState } from 'react';
 import { InputGroup, Form, Button } from "react-bootstrap";
 
 interface IInput {
@@ -7,22 +7,50 @@ interface IInput {
 
 function Input({setTodoTitle}: IInput) {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [tags, setTags] = useState([] as string[])
 
-  const handleClick = () => {
+  const tagsEndPoint = {
+    start: '',
+    end: '',
+    flag: false
+  }
+
+  const setValue = () => {
     if(inputRef.current){
       setTodoTitle(inputRef.current.value);
       inputRef.current.value = '';
+      setTags([])
     }
   }
 
   const keyPressHandler = (event: KeyboardEvent) => {
-    if(event.key === 'Enter'){
-      setTodoTitle((event.target as HTMLInputElement).value);
-      (event.target as HTMLInputElement).value = '';
+    if(inputRef.current){
+      if(event.key === '#'){
+        tagsEndPoint.start = inputRef.current.value;
+        tagsEndPoint.flag = true;
+      }
+      if(event.key === ' '){
+        if(tagsEndPoint.flag){
+          tagsEndPoint.end = inputRef.current.value
+          const tag = [tagsEndPoint.end.slice(tagsEndPoint.start.length)];
+          setTags([...tags, ...tag]);
+        }
+      }
+      if(event.key === 'Enter'){
+        if(tagsEndPoint.flag){
+          tagsEndPoint.end = inputRef.current.value
+          const tag = [tagsEndPoint.end.slice(tagsEndPoint.start.length)];
+          setTags([...tags, ...tag]);
+        }
+        setValue();
+      }
     }
+
   }
 
   return ( 
+    <>
+    <Form.Label style={{color: 'green'}}>{tags.length?`tags: ${tags.join("  ")}`:''}</Form.Label>
     <InputGroup className="mb-3">
     <Form.Control
       placeholder="Enter note title"
@@ -32,11 +60,12 @@ function Input({setTodoTitle}: IInput) {
     <Button 
     variant="outline-secondary" 
     id="button-addon2"
-    onClick={handleClick}
+    onClick={setValue}
     >
       Save
     </Button>
   </InputGroup>
+    </>
     );
 }
 
